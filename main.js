ee559809b48ff798a64595f3efb0a3b0c77e58c0
Chris Manson <chris@manson.ie>
2019-09-26 12:16:49 +0100

    adding asset size building

--- main.js
+++ main.js
@@ -3,40 +3,66 @@ import { exec } from '@actions/exec';
 import { GitHub, context } from '@actions/github';
 import assetSizeReporter from 'asset-size-reporter';
 
-
-const myToken = getInput('myToken');
-
-const octokit = new GitHub(myToken);
-
 const repoInfo = context.repo;
 
 async function run() {
-  console.log(repoInfo);
-
   try {
-    await exec('npm i');
-    await exec('npx ember build -prod');
+    const pullRequest = await getPullRequest();
 
-    let assets;
+    const prAssets = await getAssetSizes();
 
-    await assetSizeReporter({
-      patterns: ['dist/assets/**.js', 'dist/assets/**.css'],
-      json: true,
-      console: {
-        log(text) {
-          assets = JSON.parse(text);
-        }
-      },
-      cwd: process.cwd(),
-    })
+    console.log(prAssets);
 
-    console.log(assets);
+    await exec(`git checkout ${pullRequest.base.sha}`);
 
-    const myInput = getInput('myInput');
-    debug(`Hello ${myInput}`);
+    const masterAssets = await getAssetSizes();
+
+    console.log(masterAssets);
+
+    setFailed("just testing");
   } catch (error) {
     setFailed(error.message);
   }
+}
+
+async function getAssetSizes() {
+  await exec('npm ci');
+  await exec('npx ember build -prod');
+
+  let prAssets;
+
+  await assetSizeReporter({
+    patterns: ['dist/assets/**.js', 'dist/assets/**.css'],
+    json: true,
+    console: {
+      log(text) {
+        prAssets = JSON.parse(text);
+      }
+    },
+    cwd: process.cwd(),
+  })
+
+  return prAssets;
+}
+
+async function getPullRequest() {
+  let pr = context.payload.pull_request;
+
+  if (!pr) {
+    console.log('Could not get pull request number from context, exiting');
+    return;
+  }
+
+  const myToken = getInput('repo-token', { required: true });
+  const octokit = new GitHub(myToken);
+
+  const { data: pullRequest } = await octokit.pulls.get({
+    owner: pr.base.repo.owner.login,
+    repo: pr.base.repo.name,
+    pull_number: pr.number
+  })
+
+  return pullRequest;
 }
 
 export default run;
