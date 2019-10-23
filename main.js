bda0894448a53661964b00eaaf87c878b1a0852f
Chris Manson <chris@manson.ie>
2019-10-23 14:35:27 +0100

    work around issue with not having permission to comment on PRs from fork

--- main.js
+++ main.js
@@ -21,16 +21,26 @@ async function run() {
 
     const masterAssets = await getAssetSizes();
 
-    console.log({prAssets, masterAssets});
-
     let fileDiffs = diffSizes(normaliseFingerprint(masterAssets), normaliseFingerprint(prAssets));
 
-    await octokit.issues.createComment({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: pullRequest.number,
-      body: buildOutputText(fileDiffs),
-    });
+    let body = buildOutputText(fileDiffs);
+
+    try {
+      await octokit.issues.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: pullRequest.number,
+        body,
+      });
+    } catch (e) {
+      console.log(`Could not create a comment automatically. This could be because github does not allow writing from actions on a fork.
+
+See https://github.community/t5/GitHub-Actions/Actions-not-working-correctly-for-forks/td-p/35545 for more information.`);
+
+      console.log(`Copy and paste the following into a comment yourself if you want to still show the diff:
+
+${body}`);
+    }
   } catch (error) {
     setFailed(error.message);
   }
